aa84703ad0a9d662022a1dffa9dd1d8bb96fa06c
Vitor <catchtherainbow33@gmail.com>
2023-08-12 15:59:36 -0300

    Created data for knows section

---
+++ assets/js/data/know-data.js
@@ -0,0 +1,67 @@
+export const know = [
+  {
+    title: "ui/ux desing",
+    externalLink: "",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "html",
+    externalLink: "https://developer.mozilla.org/pt-BR/docs/Web/HTML",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "css",
+    externalLink: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "sass",
+    externalLink: "https://sass-lang.com/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "JavaScript ES6+",
+    externalLink: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "Typescript",
+    externalLink: "https://www.typescriptlang.org/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "Git",
+    externalLink: "https://git-scm.com/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "react",
+    externalLink: "https://react.dev/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "redux",
+    externalLink: "https://redux.js.org/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "Bootstrap",
+    externalLink: "https://getbootstrap.com/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "nodejs",
+    externalLink: "https://nodejs.org/pt-br/about",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "sql",
+    externalLink: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+  {
+    title: "Wordpress(cms)",
+    externalLink: "https://br.wordpress.org/",
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus dolor nec leo convallis sagittis. Cras commodo facilisis massa, id luctus tellus suscipit at. Aliquam rutrum rhoncus odio, pretium tempor ipsum placerat id. Donec pellentesque eros vestibulum quam placerat viverra. Ut finibus laoreet tellus eget dictum. Cras cursus, elit mattis dapibus coavallis, elit nibh vestibulu elit nibh vestibulum velit, a eleifend sapien diam accumsan risus.m velit, a eleifend sapien diam accumsan risus."
+  },
+];
