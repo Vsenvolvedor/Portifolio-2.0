b530c4095eea3c51e6ecf58b6c3ed80f91353122
Vitor <catchtherainbow33@gmail.com>
2023-08-12 14:21:15 -0300

    Setting activation of mobile menu for better performace and use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+import startMobileMenu from "./MobileMenu.js";
 import startPreload from "./Preload.js";
 
+let alreadyOn =  false;
+
+if(window.innerWidth < 720 && !alreadyOn) {
+  startMobileMenu();
+  alreadyOn = true;
+}
+
+window.onresize = () => {
+  if(window.innerWidth < 720 && !alreadyOn) {
+    startMobileMenu();
+    alreadyOn = true;
+  }
+  
+};
 startPreload();
