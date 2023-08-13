faf0835a79aeb92afb2a819d7cc2f310dda5746f
Vitor <catchtherainbow33@gmail.com>
2023-08-13 11:18:47 -0300

    Add projects base data

---
+++ assets/js/data/project-data.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: "Sakura X Clash",
+    link: "https://sakuraxclash.com/",
+    repoLink: "",
+    image: "assets/images/sakuraxclash.png",
+    about: "Site feito para o game de luta sXc. Com o objetivo de ser uma wiki e apresentar parte do projeto do game.",
+    techs: ["ui/ux desing","html", "css", "css modules", "typescript", "react", "wordpress(cms)"]
+  }
+];
