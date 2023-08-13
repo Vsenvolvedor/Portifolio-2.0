47039663a22923e266c056d66078ae7dc1178cbb
Vitor <catchtherainbow33@gmail.com>
2023-08-13 12:23:49 -0300

    Created project content renderer by data

--- assets/js/render/projectsRender.js
+++ assets/js/render/projectsRender.js
@@ -3,12 +3,36 @@ import { projects } from "../data/project-data.js";
 
 export default function start() {
   const tvNav = document.querySelector(".tv-nav ul");
+  const tvContent = document.querySelector(".tv-content");
   projects.forEach((project,index) => {
-
     tvNav.innerHTML += `
     <li class="${colorRandomizer(index)} ${index === 0 ? "active":""}" >
         <h3>${project.title}</h3>
     </li>
     `;
   });
+
+  tvContent.innerHTML = `
+  <div class="project-image">
+    <a class="project-image-link" href="${projects[0].link}" target="_blank"><img src="${projects[0].image}" alt="${projects[0].title}"></a>
+    ${projects[0].repoLink ? `
+     <a class="project-external-link"  href="${projects[0].repoLink}">Code <img src="assets/images/external-link.png" alt="external link to project"></a>
+    ` : ""}
+  </div>
+  <div class="project-info">
+    <ul class="project-tech-list">
+      ${projects[0].techs.map((tech, index) => `
+      <li class="${colorRandomizer(index)}" >
+        <h3>
+          ${tech}
+        </h3>
+      </li>
+      `)}
+    </ul>
+    <p>
+      ${projects[0].about}
+    </p>
+  </div>
+  `;
+
 };
