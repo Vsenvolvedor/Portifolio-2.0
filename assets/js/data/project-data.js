fca794906965e09ae15467a5c8dcc7833731c848
Vitor <catchtherainbow33@gmail.com>
2023-08-13 11:38:17 -0300

    Add more projects to data

--- assets/js/data/project-data.js
+++ assets/js/data/project-data.js
@@ -3,8 +3,24 @@ export const projects = [
     title: "Sakura X Clash",
     link: "https://sakuraxclash.com/",
     repoLink: "",
-    image: "assets/images/sakuraxclash.png",
+    image: "assets/images/projects/sakuraxclash.png",
     about: "Site feito para o game de luta sXc. Com o objetivo de ser uma wiki e apresentar parte do projeto do game.",
     techs: ["ui/ux desing","html", "css", "css modules", "typescript", "react", "wordpress(cms)"]
-  }
+  },
+  {
+    title: "Gestoja",
+    link: "https://github.com/vsenvolvedor/gestor-de-loja",
+    repoLink: "https://github.com/vsenvolvedor/gestor-de-loja",
+    image: "assets/images/projects/gestoja.png",
+    about: "Gestor de loja, um projeto completo. Com front end e back end funcionais.",
+    techs: ["typescript", "react", "styled components", "nodejs", "expressjs", "jwt token", "sqlite"]
+  },  
+  {
+    title: "Dogs",
+    link: "https://dogs.origamid.dev/",
+    repoLink: "https://github.com/Vsenvolvedor/Dogs",
+    image: "assets/images/projects/dogs.png",
+    about: "Projeto do curso de React da Origamid. Posteriomente com Redux implementado.O projeto se trata de uma rede social para cachorros.",
+    techs: ["css import", "css modules", "javascript", "react", "redux"]
+  },
 ];
