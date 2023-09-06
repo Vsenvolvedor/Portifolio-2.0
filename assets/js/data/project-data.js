4130054f1b68b54715d2ffea789af1d13831ddfe
Vitor <catchtherainbow33@gmail.com>
2023-09-05 22:20:44 -0300

    Add real data to projects section

--- assets/js/data/project-data.js
+++ assets/js/data/project-data.js
@@ -5,7 +5,7 @@ export const projects = [
     repoLink: "",
     image: "assets/images/projects/sakuraxclash.png",
     about: "Site feito para o game de luta sXc. Com o objetivo de ser uma wiki e apresentar parte do projeto do game.",
-    techs: ["ui/ux desing","html", "css", "css modules", "typescript", "react", "wordpress(cms)"]
+    techs: ["ui/ux design","html", "css", "css modules", "typescript", "react", "wordpress(cms)"]
   },
   {
     title: "Gestoja",
@@ -22,5 +22,101 @@ export const projects = [
     image: "assets/images/projects/dogs.png",
     about: "Projeto do curso de React da Origamid. Posteriomente com Redux implementado.O projeto se trata de uma rede social para cachorros.",
     techs: ["css import", "css modules", "javascript", "react", "redux"]
+  },
+  {
+    title: "Animais Fantásticos",
+    link: "https://vsenvolvedor.github.io/animais-fantasticos/",
+    repoLink: "https://github.com/vsenvolvedor/animais-fantasticos",
+    image: "assets/images/projects/animais-fantasticos.png",
+    about: "Projeto do curso de Javascript da Origamid. Apesar de sua aparência simples o projeto possui usos comuns do javascript para manipulação da DOM.",
+    techs: ["html", "css", "javascript"]
+  },
+  {
+    title: "Plants",
+    link: "https://vsenvolvedor.github.io/Plants/",
+    repoLink: "https://github.com/vsenvolvedor/Plants",
+    image: "assets/images/projects/plants.png",
+    about: "Com o objetivo de praticar meu UI design, plants um site fictício sobre uma escola de jardinagem. Aproveitando é claro para práticar as tecnologias também.",
+    techs: ["ui design","html", "css", "javascript"]
+  },
+  {
+    title: "Le Scone",
+    link: "https://vsenvolvedor.github.io/le-scone/",
+    repoLink: "https://github.com/Vsenvolvedor/le-scone",
+    image: "assets/images/projects/lescone.png",
+    about: "Projeto do curso de SASS da Origamid.",
+    techs: ["html", "css", "sass"]
+  },
+  {
+    title: "Gerador de Qr Code",
+    link: "https://github.com/vsenvolvedor/gerador-de-qrcode",
+    repoLink: "https://github.com/vsenvolvedor/gerador-de-qrcode",
+    image: "assets/images/projects/qrcode.png",
+    about: "Projeto bem simples, com ele consegui ter uma noção básica de como funciona o Framework Electron e como utilizar uma biblioteca externa.",
+    techs: ["html", "css", "Javascript","nodeJS","electronJS"]
+  },
+  {
+    title: "Caravan",
+    link: "https://vsenvolvedor.github.io/caravan/",
+    repoLink: "https://github.com/vsenvolvedor/caravan",
+    image: "assets/images/projects/caravan.png",
+    about: "Projeto do curso de Bootstrap da Origamid.",
+    techs: ["html", "css", "bootstrap"]
+  },
+  {
+    title: "Gerenciador de gastos",
+    link: "https://github.com/vsenvolvedor/gerenciador-de-gastos",
+    repoLink: "https://github.com/vsenvolvedor/gerenciador-de-gastos",
+    image: "assets/images/projects/gerenciador-de-gastos.png",
+    about: "Projeto desenvolvido para a prática de JavaScript, um gerenciador para ajudar você a se manter informado sobre seus gastos.",
+    techs: ["html", "css", "Javascript","NodeJS", "ExpressJS"]
+  },
+  {
+    title: "Contador",
+    link: "https://vsenvolvedor.github.io/Contador/",
+    repoLink: "https://github.com/vsenvolvedor/Contador",
+    image: "assets/images/projects/contador.png",
+    about: "Um contador simples, defina o tempo e ele começa a contar. Feito para práticar o JavaScript",
+    techs: ["html", "css", "Javascript"]
+  },
+  {
+    title: "Jogo das cores",
+    link: "https://vsenvolvedor.github.io/Colors/",
+    repoLink: "https://github.com/vsenvolvedor/Colors",
+    image: "assets/images/projects/colors.png",
+    about: "Um jogo de memorizar a ordem das cores, achei o resultado bem divertido. Feito para práticar o JavaScript.",
+    techs: ["html", "css", "Javascript"]
+  },
+  {
+    title: "Calculadora",
+    link: "https://vsenvolvedor.github.io/Calculadora/",
+    repoLink: "https://github.com/vsenvolvedor/Calculadora",
+    image: "assets/images/projects/calculadora.png",
+    about: "A clássica calculadora faz todas as operações matemáticas. Feita para treinar a lógica de programação e JavaScript.",
+    techs: ["html", "css", "Javascript"]
+  },
+  {
+    title: "Jogo da velha",
+    link: "https://vsenvolvedor.github.io/Jogo-da-velha/",
+    repoLink: "https://github.com/vsenvolvedor/Jogo-da-velha",
+    image: "assets/images/projects/jogo-da-velha.png",
+    about: "O bom e velho, jogo da velha. Feito para treinar a lógica de programação e JavaScript.",
+    techs: ["html", "css", "Javascript"]
+  },
+  {
+    title: "Cronômetro",
+    link: "https://vsenvolvedor.github.io/Cronometro/",
+    repoLink: "https://github.com/vsenvolvedor/Cronometro",
+    image: "assets/images/projects/cronometro.png",
+    about: "Um cronômetro simples. Feito para treinar a lógica de programação e JavaScript.",
+    techs: ["html", "css", "Javascript"]
+  },
+  {
+    title: "To Do List",
+    link: "https://vsenvolvedor.github.io/To-do-list/",
+    repoLink: "https://github.com/vsenvolvedor/To-do-list",
+    image: "assets/images/projects/todolist.png",
+    about: "Esse projeto auxilia você a organizar suas tarefas.",
+    techs: ["html", "css", "Javascript"]
   },
 ];
